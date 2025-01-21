68ec825e8213fa17cade2aa3e16db7902e0b6443
ibohaji <ibrahimdeiaa@gmail.com>
2025-01-21 17:00:12 +0000

    api

---
+++ src/animal_classification/api.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI 
+from contextlib import asynccontextmanager
+from fastapi import UploadFile, File
+from .model import AnimalClassifier
+import torch
+from PIL import Image
+import numpy as np 
+import torchvision.transforms as transforms
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    global model 
+    MODEL_PATH = "model.pth"
+    model = AnimalClassifier()
+    model.load_state_dict(torch.load(MODEL_PATH))
+    model.eval()
+    yield 
+    print("Shutting down...")
+    del model 
+
+
+
+def preprocess_image(image: UploadFile):
+    image = Image.open(image.file)
+    image = image.resize((48, 48))
+    if image.ndim == 3:
+        image = image.convert("L")
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: (x - x.mean()) / x.std())
+    ])
+
+    return transform(image).unsqueeze(0) # batch_dimension
+
+app  = FastAPI(lifespan=lifespan) 
+
+
+@app.get("/get_prediction")
+async def get_prediction(image: UploadFile = File(...)):
+    image = preprocess_image(image) 
+    prediction = model(image)
+    return {"prediction": prediction}
+
+
