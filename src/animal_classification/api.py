f88500b2cf9fe30a64b93004ae31329464aef733
ankevdw <ankevandewatering@gmail.com>
2025-01-24 11:07:33 +0100

    monitoring

--- src/animal_classification/api.py
+++ src/animal_classification/api.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI 
 from contextlib import asynccontextmanager
-from fastapi import UploadFile, File
+from fastapi import UploadFile, File, BackgroundTasks
 from .model import AnimalClassifier
 import torch
 from PIL import Image
 import numpy as np 
 import torchvision.transforms as transforms
+from google.cloud import storage
+import datetime
+import json
+import uuid
+from io import BytesIO
 
+BUCKET_NAME = "dtumlops_databucket"
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -50,15 +56,57 @@ def preprocess_image(image: UploadFile):
 
 app  = FastAPI(lifespan=lifespan) 
 
+def save_image_to_gcp(image: UploadFile, image_id: str):
+    """Save the uploaded image to GCP bucket."""
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    
+    # Read the image content **before** using it in a background task
+    image_content = image.file.read()  # Read the file before it gets closed
+
+    # Create an in-memory file object
+    image_buffer = BytesIO(image_content)
+
+    blob = bucket.blob(f"User_data/images/{image_id}.jpg")
+    image_buffer.seek(0) 
+    blob.upload_from_file(image_buffer, content_type=image.content_type)
+    print(f"Image {image_id}.jpg uploaded to GCP bucket.")
+
+
+def save_prediction_to_gcp(image_id: str, label: str):
+    """Save the prediction results to GCP bucket."""
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+
+    time = datetime.datetime.now(tz=datetime.timezone.utc)
+    data = {
+        "image_name": image_id,
+        "label": label,
+        "timestamp": time.isoformat(),
+    }
+
+    blob = bucket.blob(f"User_data/predictions/{image_id}.jpg")
+    blob.upload_from_string(json.dumps(data))
+    print(f"Prediction of {image_id}.jpg saved to GCP bucket.")
+
 
 @app.post("/get_prediction")
-async def get_prediction(image: UploadFile = File(...)):
-
-
-    image = preprocess_image(image) 
-    prediction = model(image)
+async def get_prediction(image: UploadFile = File(...), background_tasks: BackgroundTasks = None):
+    image_processed = preprocess_image(image) 
+    prediction = model(image_processed)
     predicted_class_idx = int(prediction.argmax())
     predicted_class = idx_to_class[predicted_class_idx]
+
+    # Store image and prediction asynchronously
+    # creating unique id for image
+    image_id = str(uuid.uuid4())
+    save_image_to_gcp(image, image_id)
+    save_prediction_to_gcp(image_id, predicted_class)
     return {"prediction": predicted_class}
 
 
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Animal Classification API!"}
